29d2b75429c4dcb7e9b4cbcadab2c6ab513f1317
williamhnyohei <williamhnyohei@gmail.com>
2023-04-27 20:19:35 -0300

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <buttonPoke
+      placeholder="capture"
+      @click="handleClick"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import buttonPoke from './components/button.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    buttonPoke,
+  },
+  methods: {
+    handleClick() {
+      console.log('capturado HAHAHAHA')
+    }
+  },
 }
 </script>
 
-<style>
+<style scoped>
+.container {
+  width: 500px;
+  height: 500px;
+}
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/button.vue
@@ -0,0 +1,98 @@
+<template>
+  <button class="pokeball-button">
+    <div class="pokeball-top"></div>
+    <div class="pokeball-bottom"></div>
+    <div class="pokeball-middle"></div>
+    <span class="pokeball-center"></span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'PokemonButton',
+  props: {
+    placeholder: String
+  },
+  methods: {
+    onClick() {
+      this.$emit('click');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.pokeball-button {
+  position: relative;
+  width: 120px;
+  height: 60px;
+  background-color: #ff3f3f;
+  border-radius: 15px 50px 50px 15px;
+  border: solid 2px black;
+  box-sizing: border-box;
+  overflow: hidden;
+}
+
+.pokeball-top, .pokeball-bottom {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 30px;
+  background-color: #fff;
+  border-radius: 15px 50px 0 0;
+}
+
+.pokeball-top {
+  box-shadow: 0 -1px 0 black;
+}
+
+.pokeball-bottom {
+  top: 30px;
+  box-shadow: 0 1px 0 black;
+}
+
+.pokeball-middle {
+  position: absolute;
+  top: 15px;
+  left: 15px;
+  width: 90px;
+  height: 30px;
+  background-color: #000;
+  border-radius: 0 50px 50px 0;
+  box-shadow: inset -2px -2px 0 rgba(255, 255, 255, 0.2);
+}
+
+.pokeball-center {
+  display: block;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 50px;
+  height: 50px;
+  background-color: #fff;
+  border-radius: 50%;
+  border: solid 2px black;
+}
+
+.pokeball-center:before, .pokeball-center:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 30px;
+  height: 2px;
+  background-color: #000;
+}
+
+.pokeball-center:before {
+  transform: translate(-50%, -50%) rotate(45deg);
+}
+
+.pokeball-center:after {
+  transform: translate(-50%, -50%) rotate(-45deg);
+}
+
+
+</style>
